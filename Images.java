a9e189411c846bc2a9091363d2a2874dd33e955e
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-12 12:13:21 -0400

    Update Images.java

--- Images.java
+++ Images.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Images {
     private URL image1;
@@ -13,30 +14,36 @@ public class Images {
     private URL image8;
     private ArrayList<ImageIcon> easyModeImages = new ArrayList<ImageIcon>();
     public Images() {
-        addImages();
+        addEasyImages();
     }
     public void urls() {
-        image1 = EasyMode.class.getClassLoader().getResource("easter basket choco bunny.jpg");
-        image2 = EasyMode.class.getClassLoader().getResource("easter basket three eggs.jpg");
-        image3 = EasyMode.class.getClassLoader().getResource("easter goofy.png");
-        image4 = EasyMode.class.getClassLoader().getResource("easter mickey mouse.png");
-        image5 = EasyMode.class.getClassLoader().getResource("easter minnie mouse.png");
-        image6 = EasyMode.class.getClassLoader().getResource("easter pluto.jpg");
-        image7 = EasyMode.class.getClassLoader().getResource("white bunny easter egg.jpg");
-        image8 = EasyMode.class.getClassLoader().getResource("white realistic bunny.png");
+        image1 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/34c8c5273925172ddeec167cb931926c.png");
+        image2 = EasyMode.class.getClassLoader().getResource("");
+        image3 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/daisy-easter.png");
+        image4 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/donald-easter5.png");
+        image5 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/goofy-easter6.png");
+        image6 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/mickey-mouse-easter5.png");
+        image7 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/minnie-mouse-easter-eggs.png");
+        image8 = EasyMode.class.getClassLoader().getResource("Images/EasyMode/minnie-pluto-easter.png");
     }
-    public void addImages() {
+    public void addEasyImages() {
         urls();
-        for (int i = 0; i < 2; i++) {
-            easyModeImages.add(new ImageIcon(image1));
-            easyModeImages.add(new ImageIcon(image2));
-            easyModeImages.add(new ImageIcon(image3));
-            easyModeImages.add(new ImageIcon(image4));
-            easyModeImages.add(new ImageIcon(image5));
-            easyModeImages.add(new ImageIcon(image6));
-            easyModeImages.add(new ImageIcon(image7));
-            easyModeImages.add(new ImageIcon(image8));
-        }
+        ImageIcon one = new ImageIcon(image1);
+        easyModeImages.addAll(Arrays.asList(one, one));
+        ImageIcon two = new ImageIcon(image2);
+        easyModeImages.addAll(Arrays.asList(two, two));
+        ImageIcon three = new ImageIcon(image3);
+        easyModeImages.addAll(Arrays.asList(three, three));
+        ImageIcon four = new ImageIcon(image4);
+        easyModeImages.addAll(Arrays.asList(four, four));
+        ImageIcon five = new ImageIcon(image5);
+        easyModeImages.addAll(Arrays.asList(five, five));
+        ImageIcon six = new ImageIcon(image6);
+        easyModeImages.addAll(Arrays.asList(six, six));
+        ImageIcon seven = new ImageIcon(image7);
+        easyModeImages.addAll(Arrays.asList(seven, seven));
+        ImageIcon eight = new ImageIcon(image8);
+        easyModeImages.addAll(Arrays.asList(eight, eight));
     }
     public ArrayList<ImageIcon> getEasyModeImages() {
         return easyModeImages;
