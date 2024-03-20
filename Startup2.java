6c4d3256fa58b7a3d13721d1cc9113d3a2cf7a72
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-20 12:33:12 -0400

    Update Startup2.java

--- Startup2.java
+++ Startup2.java
@@ -2,29 +2,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Startup {
+public class Startup2 {
     private EasyMode easyMode;
 //    private MediumMode mediumMode;
 //    private HardMode hardMode;
     private JFrame frame;
-    private JPanel panel;
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JPanel bottomPanel;
 
-    public Startup() {
+    public Startup2() {
         frame();
         easyButton();
-//        mediumButton();
-//        hardButton();
+        mediumButton();
+        hardButton();
         frame.setVisible(true);
     }
     public void frame() {
         frame = new JFrame("Memory Game");
-        frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
-        frame.getContentPane().setBackground(Color.BLACK);
-//        panel = new JPanel();
-//        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-//        JLabel label = new JLabel("MEMORY GAME", JLabel.CENTER);
-//        panel.add(label);
-        frame.getContentPane().add(panel);
+//        frame.setLayout(new GridLayout(2,1));
+//        frame.getContentPane().setBackground(Color.BLACK);
+        mainPanel = new JPanel(new BorderLayout());
+        frame.add(mainPanel);
+        topPanel = new JPanel();
+//        JLabel label = new JLabel("MEMORY GAME");
+//        label.setHorizontalAlignment(SwingConstants.CENTER);
+//        label.setVerticalAlignment(SwingConstants.BOTTOM);
+//        label.setHorizontalTextPosition(SwingConstants.CENTER);
+//        label.setVerticalTextPosition(SwingConstants.CENTER);
+//        System.out.println(label.getHorizontalAlignment());
+//        System.out.println(label.getVerticalAlignment());
+//        topPanel.add(label);
+        bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
 //        frame.setLayout(new GridBagLayout());
 //        gbc = new GridBagConstraints();
 //        gbc.anchor = GridBagConstraints.SOUTH;
@@ -39,17 +51,65 @@ public class Startup {
 //        contentPane.setLayout(new FlowLayout());
         //    private GridBagConstraints gbc;
         JButton easy = new JButton("Easy Mode");
-        easy.setBackground(Color.YELLOW);
+        easy.setBackground(Color.GREEN);
         easy.setFont(new Font("Arial", Font.PLAIN, 40));
 //        gbc.anchor = GridBagConstraints.SOUTH;
 //        gbc.weightY = 0;
 //        gbc.weightX = 0;
-        easy.setBounds(275, 570, 450, 90);
-        frame.add(easy, BorderLayout.CENTER);
+        easy.setPreferredSize(new Dimension(570, 100));
+        easy.setBorderPainted(false);
+//        easy.setBounds(275, 570, 450, 90);
+        bottomPanel.add(easy, BorderLayout.NORTH);
 //        panel.add(easy, BorderLayout.CENTER);
 //        contentPane.setSize(900,900);
 //        contentPane.setVisible(true)
         easy.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                easyMode = new EasyMode();
+            }
+        });
+    }
+    public void mediumButton() {
+//        Container contentPane = frame.getContentPane();
+//        contentPane.setLayout(new FlowLayout());
+        //    private GridBagConstraints gbc;
+        JButton medium = new JButton("Medium Mode");
+        medium.setBackground(Color.YELLOW);
+        medium.setFont(new Font("Arial", Font.PLAIN, 40));
+//        gbc.anchor = GridBagConstraints.SOUTH;
+//        gbc.weightY = 0;
+//        gbc.weightX = 0;
+        medium.setPreferredSize(new Dimension(570, 100));
+        medium.setBorderPainted(false);
+//        easy.setBounds(275, 570, 450, 90);
+        bottomPanel.add(medium, BorderLayout.CENTER);
+//        panel.add(easy, BorderLayout.CENTER);
+//        contentPane.setSize(900,900);
+//        contentPane.setVisible(true)
+        medium.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                easyMode = new EasyMode();
+            }
+        });
+    }
+    public void hardButton() {
+//        Container contentPane = frame.getContentPane();
+//        contentPane.setLayout(new FlowLayout());
+        //    private GridBagConstraints gbc;
+        JButton hard = new JButton("Hard Mode");
+        hard.setBackground(Color.RED);
+        hard.setFont(new Font("Arial", Font.PLAIN, 40));
+//        gbc.anchor = GridBagConstraints.SOUTH;
+//        gbc.weightY = 0;
+//        gbc.weightX = 0;
+        hard.setPreferredSize(new Dimension(570, 100));
+        hard.setBorderPainted(false);
+//        easy.setBounds(275, 570, 450, 90);
+        bottomPanel.add(hard, BorderLayout.SOUTH);
+//        panel.add(easy, BorderLayout.CENTER);
+//        contentPane.setSize(900,900);
+//        contentPane.setVisible(true)
+        hard.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
                 easyMode = new EasyMode();
             }
