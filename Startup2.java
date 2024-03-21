34c8ac91a54aed1dfa5f0fec4f38df236a27f515
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-21 12:31:59 -0400

    Update Startup2.java

--- Startup2.java
+++ Startup2.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Startup2 {
+public class Startup {
     private EasyMode easyMode;
 //    private MediumMode mediumMode;
 //    private HardMode hardMode;
@@ -11,7 +11,7 @@ public class Startup2 {
     private JPanel topPanel;
     private JPanel bottomPanel;
 
-    public Startup2() {
+    public Startup() {
         frame();
         easyButton();
         mediumButton();
@@ -20,28 +20,30 @@ public class Startup2 {
     }
     public void frame() {
         frame = new JFrame("Memory Game");
-//        frame.setLayout(new GridLayout(2,1));
-//        frame.getContentPane().setBackground(Color.BLACK);
         mainPanel = new JPanel(new BorderLayout());
         frame.add(mainPanel);
         topPanel = new JPanel();
-//        JLabel label = new JLabel("MEMORY GAME");
-//        label.setHorizontalAlignment(SwingConstants.CENTER);
-//        label.setVerticalAlignment(SwingConstants.BOTTOM);
-//        label.setHorizontalTextPosition(SwingConstants.CENTER);
-//        label.setVerticalTextPosition(SwingConstants.CENTER);
-//        System.out.println(label.getHorizontalAlignment());
-//        System.out.println(label.getVerticalAlignment());
-//        topPanel.add(label);
+        JButton button = new JButton();
+        button.setPreferredSize(new Dimension(100,500));
+        button.setEnabled(false);
+        button.setBorderPainted(false);
+        button.setBackground(new Color(153,153,255));
+        topPanel.add(button);
+        topPanel.setBackground(new Color(153,153,255));
         bottomPanel = new JPanel();
-        bottomPanel.setLayout(new BorderLayout());
+        bottomPanel.setLayout(new BorderLayout(0,25));
+        bottomPanel.setBackground(new Color(153,153,255));
+        JPanel extraPanel = new JPanel();
+        JButton button2 = new JButton();
+        button2.setPreferredSize(new Dimension(100,50));
+        button2.setEnabled(false);
+        button2.setBorderPainted(false);
+        button2.setBackground(new Color(153,153,255));
+        extraPanel.add(button2);
+        extraPanel.setBackground(new Color(153,153,255));
         mainPanel.add(topPanel, BorderLayout.NORTH);
-        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
-//        frame.setLayout(new GridBagLayout());
-//        gbc = new GridBagConstraints();
-//        gbc.anchor = GridBagConstraints.SOUTH;
-//        gbc.weightY = 0;
-//        gbc.weightX = 0;
+        mainPanel.add(bottomPanel, BorderLayout.CENTER);
+        mainPanel.add(extraPanel, BorderLayout.SOUTH);
         frame.setSize(1000, 1000);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,12 +53,12 @@ public class Startup2 {
 //        contentPane.setLayout(new FlowLayout());
         //    private GridBagConstraints gbc;
         JButton easy = new JButton("Easy Mode");
-        easy.setBackground(Color.GREEN);
+        easy.setBackground(new Color(187, 255, 153));
         easy.setFont(new Font("Arial", Font.PLAIN, 40));
 //        gbc.anchor = GridBagConstraints.SOUTH;
 //        gbc.weightY = 0;
 //        gbc.weightX = 0;
-        easy.setPreferredSize(new Dimension(570, 100));
+        easy.setPreferredSize(new Dimension(570, 115));
         easy.setBorderPainted(false);
 //        easy.setBounds(275, 570, 450, 90);
         bottomPanel.add(easy, BorderLayout.NORTH);
@@ -74,12 +76,12 @@ public class Startup2 {
 //        contentPane.setLayout(new FlowLayout());
         //    private GridBagConstraints gbc;
         JButton medium = new JButton("Medium Mode");
-        medium.setBackground(Color.YELLOW);
+        medium.setBackground(new Color(255, 221, 153));
         medium.setFont(new Font("Arial", Font.PLAIN, 40));
 //        gbc.anchor = GridBagConstraints.SOUTH;
 //        gbc.weightY = 0;
 //        gbc.weightX = 0;
-        medium.setPreferredSize(new Dimension(570, 100));
+        medium.setPreferredSize(new Dimension(570, 115));
         medium.setBorderPainted(false);
 //        easy.setBounds(275, 570, 450, 90);
         bottomPanel.add(medium, BorderLayout.CENTER);
@@ -97,12 +99,12 @@ public class Startup2 {
 //        contentPane.setLayout(new FlowLayout());
         //    private GridBagConstraints gbc;
         JButton hard = new JButton("Hard Mode");
-        hard.setBackground(Color.RED);
+        hard.setBackground(new Color(255, 153, 204));
         hard.setFont(new Font("Arial", Font.PLAIN, 40));
 //        gbc.anchor = GridBagConstraints.SOUTH;
 //        gbc.weightY = 0;
 //        gbc.weightX = 0;
-        hard.setPreferredSize(new Dimension(570, 100));
+        hard.setPreferredSize(new Dimension(570, 115));
         hard.setBorderPainted(false);
 //        easy.setBounds(275, 570, 450, 90);
         bottomPanel.add(hard, BorderLayout.SOUTH);
