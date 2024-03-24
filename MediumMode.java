3ef1f407fb39e36fa18e300016b9dbd5d85dbf62
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-24 01:34:27 -0400

    Update MediumMode.java

--- MediumMode.java
+++ MediumMode.java
@@ -1,5 +1,9 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
 public class MediumMode extends Mode{
-    public MediumMode() {
+    public MediumMode() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         super("medium",4,5);
     }
 }
