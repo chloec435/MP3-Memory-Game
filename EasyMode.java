0d1a142f7c1377c0e0f4d612363d422b6ac9f06b
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-24 01:33:04 -0400

    Update EasyMode.java

--- EasyMode.java
+++ EasyMode.java
@@ -1,5 +1,10 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
 public class EasyMode extends Mode{
-    public EasyMode() {
+    public EasyMode() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+
         super("easy",3,4);
     }
 }
