147dc9fdb6aec7d1b03f2239c3d817d12c2ac2a7
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-27 00:48:19 -0400

    Update Images.java

--- Images.java
+++ Images.java
@@ -68,34 +68,34 @@ public class Images {
         hardModeURLs.addAll(Arrays.asList(hImage1, hImage2, hImage3, hImage4, hImage5, hImage6, hImage7,
                 hImage8, hImage9, hImage10, hImage11, hImage12, hImage13, hImage14, hImage15));
 
-        URL iImage1 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Gary_the_Snail.png");
-        URL iImage2 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Mr._Krabs.svg.png");
-        URL iImage3 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Mrs._Puff.svg.png");
-        URL iImage4 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Patrick_Star.svg.png");
-        URL iImage5 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Pearl_the_Whale.svg.png");
-        URL iImage6 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Plankton_and_Karen.svg.png");
-        URL iImage7 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Sandy_Cheeks.svg.png");
-        URL iImage8 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-gary.png");
-        URL iImage9 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob.png");
-        URL iImage10 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/patrick.png");
-        URL iImage11 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-yay.png");
-        URL iImage12 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-good.png");
-        URL iImage13 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-and-friends.png");
-        URL iImage14 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/SpongeBob_SquarePants_character.svg.png");
-        URL iImage15 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Squidward_Tentacles.svg.png");
-        URL iImage16 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Gary_the_Snail.png");
-        URL iImage17 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Mr._Krabs.svg.png");
-        URL iImage18 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Mrs._Puff.svg.png");
-        URL iImage19 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Patrick_Star.svg.png");
-        URL iImage20 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Pearl_the_Whale.svg.png");
-        URL iImage21 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Plankton_and_Karen.svg.png");
-        URL iImage22 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/Sandy_Cheeks.svg.png");
-        URL iImage23 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-gary.png");
-        URL iImage24 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob.png");
-        URL iImage25 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/patrick.png");
-        URL iImage26 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-yay.png");
-        URL iImage27 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-good.png");
-        URL iImage28 = ImpossibleMode.class.getClassLoader().getResource("Images/HardMode/spongebob-and-friends.png");
+        URL iImage1 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Abe_Simpson.png");
+        URL iImage2 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Apu_Nahasapeemapetilon_(The_Simpsons).png");
+        URL iImage3 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Barney_Gumble.png");
+        URL iImage4 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Bart_Simpson_200px.png");
+        URL iImage5 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Chief_Wiggum.png");
+        URL iImage6 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Dr._Hibbert.svg.png");
+        URL iImage7 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Edna_Krabappel.png");
+        URL iImage8 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/FatTony.png");
+        URL iImage9 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Frink.png");
+        URL iImage10 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/GroundskeeperWillie.png");
+        URL iImage11 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Homer_Simpson_2006.png");
+        URL iImage12 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Krustytheclown.png");
+        URL iImage13 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Lisa_Simpson.png");
+        URL iImage14 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Maggie_Simpson.png");
+        URL iImage15 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Marge_Simpson.png");
+        URL iImage16 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Mayor_Quimby.png");
+        URL iImage17 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Milhouse_Van_Houten.png");
+        URL iImage18 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Moe_Szyslak.png");
+        URL iImage19 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Mr_Burns.png");
+        URL iImage20 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Ned_Flanders.png");
+        URL iImage21 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Nelson_Muntz.png");
+        URL iImage22 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Patty_Bouvier.png");
+        URL iImage23 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Ralph_Wiggum.png");
+        URL iImage24 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Rev_Lovejoy.png");
+        URL iImage25 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Selma_Bouvier.png");
+        URL iImage26 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Seymour_Skinner.png");
+        URL iImage27 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/The_Simpsons-Jeff_Albertson.png");
+        URL iImage28 = ImpossibleMode.class.getClassLoader().getResource("Images/ImpossibleMode/Waylon_Smithers_1.png");
         impossibleModeURLs.addAll(Arrays.asList(iImage1, iImage2, iImage3, iImage4, iImage5, iImage6, iImage7, iImage8,
                 iImage9, iImage10, iImage11, iImage12, iImage13, iImage14, iImage15, iImage16, iImage17, iImage18,
                 iImage19, iImage20, iImage21, iImage22, iImage23, iImage24, iImage25, iImage26, iImage27, iImage28));
@@ -119,8 +119,8 @@ public class Images {
         }
     }
     public void addImpossibleImages() {
-        for (int i = 0; i < 15; i++) {
-            ImageIcon icon = new ImageIcon(getScaled(new ImageIcon(hardModeURLs.get(i)), 175, 200));
+        for (int i = 0; i < 28; i++) {
+            ImageIcon icon = new ImageIcon(getScaled(new ImageIcon(impossibleModeURLs.get(i)), 175, 200));
             impossibleModeImages.addAll(Arrays.asList(icon, icon));
         }
     }
