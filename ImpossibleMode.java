56346b7dd3ca49e13372a96eec77326a8f97146b
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-26 22:04:21 -0400

    Create ImpossibleMode.java

---
+++ ImpossibleMode.java
@@ -0,0 +1,9 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+public class ImpossibleMode extends Mode {
+    public ImpossibleMode() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        super("impossible",8,8);
+    }
+}
