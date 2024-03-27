5106333e483cd373b382f653fe33830859573bf6
chloec435 <144473427+chloec435@users.noreply.github.com>
2024-03-27 00:47:47 -0400

    Update Startup.java

--- Startup.java
+++ Startup.java
@@ -1,8 +1,11 @@
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -10,7 +13,10 @@ public class Startup {
     private JFrame frame;
     private JPanel bottomPanel;
     private JPanel extraPanel;
-    public Startup() {
+    private Clip opening;
+    public Startup(boolean tOF) {
+    }
+    public Startup() throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         frame();
         easyButton();
         mediumButton();
@@ -18,7 +24,7 @@ public class Startup {
         impossibleButton();
         frame.setVisible(true);
     }
-    public void frame() {
+    public void frame() throws LineUnavailableException, UnsupportedAudioFileException, IOException {
         frame = new JFrame("Memory Game");
         JPanel mainPanel = new JPanel(new BorderLayout());
         frame.add(mainPanel);
@@ -43,9 +49,14 @@ public class Startup {
         mainPanel.add(topPanel, BorderLayout.NORTH);
         mainPanel.add(bottomPanel, BorderLayout.CENTER);
         mainPanel.add(extraPanel, BorderLayout.SOUTH);
-        frame.setSize(1250, 1000);
+        frame.setSize(1100, 1000);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        opening = AudioSystem.getClip();
+        opening.open(AudioSystem.getAudioInputStream(new File("Audio/Background Cartoon Music Loop ｜ (Download and Royalty FREE).wav")));
+        opening.loop(Clip.LOOP_CONTINUOUSLY);
+        opening.start();
     }
     public void easyButton() {
         JButton easy = new JButton();
@@ -62,6 +73,7 @@ public class Startup {
             @Override
             public void actionPerformed(ActionEvent e){
                 frame.dispose();
+                opening.stop();
                 try {
                     EasyMode easyMode = new EasyMode();
                 } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
@@ -85,6 +97,7 @@ public class Startup {
             @Override
             public void actionPerformed(ActionEvent e){
                 frame.dispose();
+                opening.stop();
                 try {
                     MediumMode mediumMode = new MediumMode();
                 } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
@@ -108,6 +121,7 @@ public class Startup {
             @Override
             public void actionPerformed(ActionEvent e){
                 frame.dispose();
+                opening.stop();
                 try {
                     HardMode hardMode = new HardMode();
                 } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
@@ -121,7 +135,7 @@ public class Startup {
         impossible.setLayout(new BorderLayout());
         JLabel impossibleModeImage = new JLabel(new ImageIcon(new ImageIcon
                 (Objects.requireNonNull(Startup.class.getClassLoader().getResource("Images/Startup/ImpossibleMode.png")))
-                .getImage().getScaledInstance(700, 150, Image.SCALE_SMOOTH)));
+                .getImage().getScaledInstance(900, 175, Image.SCALE_SMOOTH)));
         impossible.add(impossibleModeImage, BorderLayout.CENTER);
         impossible.setBackground(new Color(159, 210, 249));
         impossible.setPreferredSize(new Dimension(1250, 115));
@@ -131,8 +145,11 @@ public class Startup {
             @Override
             public void actionPerformed(ActionEvent e){
                 frame.dispose();
+                opening.stop();
                 try {
                     ImpossibleMode impossibleMode = new ImpossibleMode();
+                    JOptionPane.showMessageDialog(frame, "Maximize the window to see full images.",
+                            "Maximize Window", JOptionPane.INFORMATION_MESSAGE);
                 } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ex) {
                     throw new RuntimeException(ex);
                 }
